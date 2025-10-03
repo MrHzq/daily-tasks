b8a5c4f9c5fbe0f682b188c3948e77485bdc25e9
huangzq <13402318457@163.com>
2025-10-03 13:16:24 +0800

    refactor: 重命名应用并更新欢迎信息
    将应用从 random-number-system 重命名为 daily-tasks，并更新相关描述文本
    移除未使用的组件和图标

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,13 @@
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
   <main class="flex justify-center items-center flex-col px-[20px] gap-4">
-    <div class="text-xl text-sky-500">欢迎使用 random-number-system</div>
+    <div class="text-xl text-sky-500">欢迎使用 daily-tasks</div>
     <p class="text-sm text-gray-500">
-      它可以自动生成给定范围内的随机正整数，可用帮你打乱顺序记东西：比如记英语单词~
+      它可以用来管理你的每日任务，比如记录待办事项、提醒事项、计划任务等。
     </p>
   </main>
-  <CusSwitch />
-
-  <ArrowDownCircleIcon class="w-6 h-6" />
-
   <ChatBubbleBottomCenterIcon class="w-16 h-16" />
-
-  <el-button type="primary">主要按钮</el-button>
 </template>
 
-<script setup lang="ts">
-import CusSwitch from './CusSwitch.vue'
-
-import { ArrowDownCircleIcon } from '@heroicons/vue/16/solid'
-import { ChatBubbleBottomCenterIcon } from '@heroicons/vue/24/outline'
-</script>
+<script setup lang="ts"></script>
 
 <style scoped></style>
