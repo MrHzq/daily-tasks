e1a2bcb69af24d16af6f8579ef4d030b1ce03962
huangzq <13402318457@163.com>
2025-10-03 18:14:02 +0800

    style: 调整全局布局样式以包含页脚
    修改 reset.css 和 App.vue 的布局结构，确保应用容器和子元素高度正确
    添加页脚区域并设置样式，保持整体布局的一致性

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <template>
-  <router-view></router-view>
+  <div class="flex overflow-hidden flex-col bg-white">
+    <!-- 内容 -->
+    <div class="overflow-hidden flex-1 w-full">
+      <router-view class="h-full"></router-view>
+    </div>
+
+    <!-- 页脚 -->
+    <footer class="flex-none px-4 py-6 w-full shadow-inner">
+      <div class="text-sm text-center text-gray-500">
+        <p>Daily Tasks &copy; 2025</p>
+      </div>
+    </footer>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
