6fff2e5a1525d5c3a29a50b3f4152aa441a408f5
huangzq <13402318457@163.com>
2025-10-03 13:12:16 +0800

    chore: 安装 heroicons

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,22 @@
   </p>
   <main class="flex justify-center items-center flex-col px-[20px] gap-4">
     <div class="text-xl text-sky-500">欢迎使用 random-number-system</div>
-    <p class="text-gray-500 text-sm">
+    <p class="text-sm text-gray-500">
       它可以自动生成给定范围内的随机正整数，可用帮你打乱顺序记东西：比如记英语单词~
     </p>
   </main>
   <CusSwitch />
+
+  <ArrowDownCircleIcon class="w-6 h-6" />
+
+  <ChatBubbleBottomCenterIcon class="w-16 h-16" />
 </template>
 
 <script setup lang="ts">
 import CusSwitch from './CusSwitch.vue'
+
+import { ArrowDownCircleIcon } from '@heroicons/vue/16/solid'
+import { ChatBubbleBottomCenterIcon } from '@heroicons/vue/24/outline'
 </script>
 
 <style scoped></style>
