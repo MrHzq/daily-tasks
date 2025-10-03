b4ea20335295e1e320a5244cf1c5ca44e162b706
huangzq <13402318457@163.com>
2025-10-03 13:06:15 +0800

    chore: 安装tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,12 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <main class="flex justify-center items-center flex-col px-[20px] gap-4">
+    <div class="text-xl text-sky-500">欢迎使用 random-number-system</div>
+    <p class="text-gray-500 text-sm">
+      它可以自动生成给定范围内的随机正整数，可用帮你打乱顺序记东西：比如记英语单词~
+    </p>
+  </main>
 </template>
 
 <style scoped></style>
