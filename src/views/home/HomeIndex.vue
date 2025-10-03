c332b624607e0e8252ff5f89ed1a61e0c8ecada1
huangzq <13402318457@163.com>
2025-10-03 19:05:23 +0800

    feat(首页): 添加开始使用按钮并优化布局
    添加开始使用按钮跳转至每日任务页面
    优化首页布局和文字描述

--- src/views/home/HomeIndex.vue
+++ src/views/home/HomeIndex.vue
@@ -1,10 +1,17 @@
 <template>
-  <main class="home flex justify-center items-center flex-col px-[20px] gap-4">
-    <div class="text-xl text-sky-500">欢迎使用 daily-tasks</div>
-    <p class="text-sm text-gray-500">
-      它可以用来管理你的每日任务，比如记录待办事项、提醒事项、计划任务等。
-    </p>
+  <main class="flex flex-col gap-4 items-center pt-6 home">
+    <div class="text-xl text-sky-500">欢迎使用 Daily Tasks</div>
+    <p class="text-sm text-gray-500">它可以用来管理你的每日安排</p>
+    <el-button type="primary" @click="handleStartClick">开始使用</el-button>
   </main>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const handleStartClick = () => {
+  router.push('/daily')
+}
+</script>
