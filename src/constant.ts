8eecd4c4219553c17d3f7601086e643ebce590ea
huangzq <13402318457@163.com>
2025-10-05 15:36:37 +0800

    refactor(assets): 重构资源文件结构并更新引用路径
    将reset.css和新增的importTailwindcss.css移至assets目录
    删除不再使用的constant.ts文件
    更新main.ts中的资源引用路径

--- src/constant.ts
+++
@@ -1,1 +0,0 @@
-export const belongList = ['both', 'workday', 'weekend']
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
-import './reset.css'
-import './index.css'
+import './assets/reset.css'
+import './assets/importTailwindcss.css'
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
-import '@/utils/holidayChecker'
+import './utils/holidayChecker'
 
 import App from './App.vue'
 import router from './router'
